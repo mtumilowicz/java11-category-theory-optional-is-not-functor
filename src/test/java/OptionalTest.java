ce1f7865ea3e65bc9e7acb2d09facdf57ffd90dc
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-28 19:49:00 +0100

    optional as stream

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -5,8 +5,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 import static java.util.Objects.nonNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by mtumilowicz on 2018-12-28.
@@ -16,14 +15,22 @@ public class OptionalTest {
     private Function<Integer, Integer> nullFunction = i -> null;
     private Function<Integer, String> toString = i -> nonNull(i) ? String.valueOf(i) : "null";
     private Function<Integer, String> composition = nullFunction.andThen(toString);
-    
+
     @Test
     public void breaksCompositionRules() {
-        assertNotEquals(Optional.of(1).map(composition), Optional.of(1).map(nullFunction).map(toString));
+        assertNotEquals(Optional.of(1).map(composition),
+                Optional.of(1).map(nullFunction).map(toString));
+    }
+
+    @Test
+    public void followsCompositionRulesAsStream() {
+        assertEquals(Optional.of(1).stream().map(composition).findAny(),
+                Optional.of(1).stream().map(nullFunction).map(toString).findAny());
     }
 
     @Test
     public void followsCompositionRules() {
-        assertEquals(Option.of(1).map(composition), Option.of(1).map(nullFunction).map(toString));
+        assertEquals(Option.of(1).map(composition),
+                Option.of(1).map(nullFunction).map(toString));
     }
 }
